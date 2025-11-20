141e0abcd003e680eb96f47e49f2b5b07e837979
Kamyin420 <96398575+Kamyin420@users.noreply.github.com>
2025-11-20 16:17:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".navigatie");
+const title = document.querySelector("header h3");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    title.classList.toggle("hide");
+});
